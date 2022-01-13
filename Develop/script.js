f47c8aec5422503bf64c4347d881f36477b62d5f
Evan <evanpauly@hotmail.com>
2022-01-13 16:40:02 -0600

    still working on randomizing and getting an output

--- Develop/script.js
+++ Develop/script.js
@@ -1,36 +1,40 @@
 // Assignment code here
-
+function generatePassword() {
   var lowercase = "abcdefghijklmnopqrstuvwxyz";
   var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var numbers = "0123456789";
-  var specialCharachters = "!#$%'()*+,-./:;<=>?@[\]^_`{|}~";
+  var specialCharacters = "!#$%'()*+,-./:;<=>?@[\]^_`{|}~";
+  var passwordLength = "";
+
+    var passwordLength = window.prompt("Password Length");
+
+    if (passwordLength < 8 || passwordLength > 128) {
+      window.alert("Password Length must be between 8 and 128.")
+      return generatePassword();
+    }
   
-function generatePassword() {
-  var passwordLength = window.prompt("Password Length");
-
-  if (passwordLength < 8 || passwordLength > 128) {
-    window.alert("Password Length must be between 8 and 128.")
-    return generatePassword();
-  }
-
   var lowercase = window.confirm("Lowercase Letters?");
+
+  if (lowercase === true) {
+    return lowercase[Math.floor(Math.random() * lowercase.length)]
+  };
+
   var uppercase = window.confirm("Uppercase Letters?");
   var numbers = window.confirm("Numbers?");
-  var specialCharachters = window.confirm("Special Charachters?")
+  var specialCharacters = window.confirm("Special Charachters?")
 
-  if (lowercase === false && uppercase === false && numbers === false && specialCharachters === false) {
+  if (lowercase === false && uppercase === false && numbers === false && specialCharacters === false) {
     window.alert("You must select at least one option.")
     return generatePassword();
   };
-
-  //3. generate password based on criteria
-  for (var i = 0; i <= password; i++) {
-    var password = Math.floor(Math.random() )
-  }
-
   
+  //3. generate password based on criteria
+  for (var i = 0; i < passwordLength; i++) {
+    var passwordLength = Math.floor(Math.random() * passwordLength);
+  };
+
   //4. display password to the page
-  return passwordText;
+  return password;
 };
 
 generatePassword();
@@ -39,7 +43,7 @@ generatePassword();
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
+function writePassword(passwordLength, lowercase, uppercase, numbers, specialCharacters) {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -50,3 +54,4 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+writePassword();
